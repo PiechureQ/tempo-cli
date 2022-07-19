0df5902c9d2b48d8d1b36fc0dd6a30ce835a6955
Michał Piechura <piechureq@wp.pl>
2022-07-20 00:17:55 +0200

    init

---
+++ tempo.py
@@ -0,0 +1,8 @@
+import argparse
+import logging 
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Log time to Tempo 😎")
+    args = parser.parse_args()
+
+logging.warning('just works')
