2443f3dfe6aa1d5cb2e79c7d223006b8d7b682d4
Michał Piechura <piechureq@wp.pl>
2022-07-21 00:26:57 +0200

    log 8h to selected task today

--- tempo-cli.py
+++ tempo-cli.py
@@ -1,13 +1,20 @@
 import argparse
+import logging
 from config import load_config 
 from tempo import Tempo
+from datetime import date
+
+def log_time(tempo, task_id, date=str(date.today()), time=28800):
+    tempo.post_worklog(task_id, date, time)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Log time to Tempo 😎")
+    parser.add_argument('task', metavar='task_id', type=str, help='Jira task id')
     args = parser.parse_args()
+    print()
 
     CONFIG = load_config()
 
-    tempo = Tempo(CONFIG.get('user', 'token'), CONFIG.get('user', 'url'))
+    tempo = Tempo(CONFIG.get('user', 'token'), CONFIG.get('user', 'account_id'), logging.DEBUG)
 
-    tempo.test()
+    log_time(tempo, args.task)
--- tempo.py
+++ tempo.py
@@ -1,13 +1,39 @@
 import requests
+import logging
+
+TEMPO_URL = 'https://api.tempo.io/4'
+
+def urljoin(base, path):
+    return base + path
 
 class Tempo:
-    def __init__(self, token, url):
+    def __init__(self, token, account_id, logging_level=logging.INFO):
         self.header = {
             "Authorization": f"Bearer {token}"
         }
-        self.baseUrl = url
+        self.account_id = account_id
+        self.baseUrl = TEMPO_URL
         self.session = requests.Session()
         self.session.headers.update(self.header)
+        self.logging_level = logging_level
 
-    def test(self):
-        print('Tempo class test')
+    def __api_post(self, path, data=None, headers=None, params=None, json=None, raise_for_status=True):
+        logging.getLogger("tempo").setLevel(self.logging_level)
+        url = urljoin(self.baseUrl, path)
+        response = self.session.post(url, data=data, headers=headers, params=params, json=json, timeout=3)
+        if raise_for_status:
+            response.raise_for_status()
+        logging.debug('post')
+        return response.json()
+
+    def post_worklog(self, issue_key, date, time):
+        payload = {
+            "authorAccountId": self.account_id,
+            "issueKey": issue_key,
+            "startDate": date,
+            "timeSpentSeconds": time,
+            "description": "Tempo cli tool"
+        }
+
+        return self.__api_post('/worklogs', json=payload)
+
